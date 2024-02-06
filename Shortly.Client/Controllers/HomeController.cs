f1e7dd24672ae2ad92b4c6c34e83e8b43dd62986
Nader <developernader@gmail.com>
2024-02-06 11:40:43 +0300

    Added Url controller

--- Shortly.Client/Controllers/HomeController.cs
+++ Shortly.Client/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Shortly.Client.Models;
 using System.Diagnostics;
 
 namespace Shortly.Client.Controllers
@@ -23,10 +22,6 @@ namespace Shortly.Client.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      
     }
 }
---
+++ Shortly.Client/Data/Models/Url.cs
@@ -0,0 +1,14 @@
+﻿namespace Shortly.Client.Data.Models
+{
+    public class Url
+    {
+        public int Id { get; set; }
+        public string OriginalLink { get; set; }
+        public string ShortLik { get; set; }
+        public int NrOfClicks { get; set; }
+        public int? UserId { get; set; }
+
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
